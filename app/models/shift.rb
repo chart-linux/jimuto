a3f4d7583ad7aa2ce07d2d2ba1ee8fb7740b07ac
柴田佳祐 <keisuke.shibata@happyelements.co.jp>
2016-08-28 03:12:36 +0900

    点数計算式の変更

--- app/models/shift.rb
+++ app/models/shift.rb
@@ -4,11 +4,7 @@ class Shift < ApplicationRecord
 
   def score_by_popularity
     # TODO: この分岐をしなくていいように（windowの締め切り機能を作れば良い https://github.com/chart-linux/jimuto/issues/7 ）
-    if popularity > 0
-      1.to_f / popularity
-    else
-      0
-    end
+    Math.exp(-popularity)
   end
 
   def popularity
