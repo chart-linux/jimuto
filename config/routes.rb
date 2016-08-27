e4daa25ebb3d49aaad3401c23fcb3a607327efaa
Hayato Hashimoto <hayato.hashimoto@gmail.com>
2016-08-28 03:49:09 +0900

    added user page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
+  get 'users/index'
+
+  get 'users/show'
+
   devise_for :users
+  resources :users, :only => [:index, :show]
 
   root to: "windows#index"
   put 'api/:controller/:action'
