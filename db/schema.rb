2cd2fab389285f4182b2ad0070cd0d0f1553622e
柴田佳祐 <keisuke.shibata@happyelements.co.jp>
2016-08-27 17:28:17 +0900

    solution belongs to window

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160827074536) do
+ActiveRecord::Schema.define(version: 20160827082424) do
 
   create_table "request_solutions", force: :cascade do |t|
     t.datetime "created_at",  null: false
@@ -44,7 +44,9 @@ ActiveRecord::Schema.define(version: 20160827074536) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer  "user_id"
+    t.integer  "window_id"
     t.index ["user_id"], name: "index_solutions_on_user_id"
+    t.index ["window_id"], name: "index_solutions_on_window_id"
   end
 
   create_table "users", force: :cascade do |t|
