b0db01f9ddd0223f4a904f8f320462bde329b95e
柴田佳祐 <keisuke.shibata@happyelements.co.jp>
2016-08-28 11:17:38 +0900

    シードデータ

--- db/seeds/except_requests.rb
+++ db/seeds/except_requests.rb
@@ -26,6 +26,29 @@ user = User.new email: 'kumano@kumano-ryo.com', password: 'password', name: '熊
 user.skip_confirmation!
 user.save!
 
+
+time = Date.today.next_week.to_time
+5.times do |num|
+  window = Window.create(message: %W(A B C D E F G H).sample + "班 当番", status: :requesting)
+  7.times do |week_num|
+      [[[hour: 8], [hour: 10]], [[hour: 10], [hour: 12, minute: 30]],[ [hour: 12, minute: 30], [hour: 15]],[[hour: 15], [hour: 17, minute: 30]],[[hour: 17, minute: 30],[hour: 20]],[[hour: 20], [hour: 23]]].each do |options|
+      Shift.create!(
+        start: time.change(options[0][0]),
+        end: time.change(options[1][0]),
+        window: window
+      )
+    end
+      Shift.create!(
+        start: time.change(hour: 23),
+        end: time.tomorrow.change(hour: 8),
+        window: window
+      )
+    time = time.tomorrow
+  end
+end
+
+
+=begin
 window = Window.create! message: Faker::Hacker.say_something_smart
 
 # generate shift
@@ -43,3 +66,5 @@ window = Window.create! message: Faker::Hacker.say_something_smart
     window: window
   )
 end
+
+=end
